bfc38a5cf468e2ce97141cff34df434edb75ce81
Diogo Carlos Saramago Marques <diocar24@hotmail.com>
2017-12-10 14:27:59 +0000

    Chapter5_Razor: solution start setup

---
+++ Chapter5_Razor/Chapter5_Razor/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Chapter5_Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter5_Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID   = 1,
+            Name        = "Kayak",
+            Description = "A boat for one person",
+            Category    = "Watersports",
+            Price       = 275M
+        };
+
+        // GET: Hom
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
