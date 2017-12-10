a780b382458afca2f038e4c0976005a94d44db0b
Diogo Carlos Saramago Marques <diocar24@hotmail.com>
2017-12-10 11:26:31 +0000

    Chapter4_LanguageFeatures: solution start setup

---
+++ Chapter4_LanguageFeatures/Chapter4_LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter4_LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        //// GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
