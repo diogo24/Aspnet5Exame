7e01e9e387afb93654b64a172dfe7e5b0ec0eb83
Diogo Carlos Saramago Marques <diocar24@hotmail.com>
2017-12-10 12:13:05 +0000

    1) Using Extension Methods: 1.b) Applying Extension Methods to an Interface 1.c) Creating Filtering Extension Methods

---
+++ Chapter4_LanguageFeatures/Chapter4_LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chapter4_LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cartParam) {
+            decimal total = 0;
+
+            foreach (Product prod in cartParam.Products)
+            {
+                total += prod.Price;
+            }
+
+            return total;
+        }
+
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+
+            foreach (Product prod in productEnum)
+            {
+                total += prod.Price;
+            }
+
+            return total;
+        }
+
+        public static IEnumerable<Product> FilterByCategory(this IEnumerable<Product> prodEnum, string categoryParam) {
+            foreach (var prod in prodEnum)
+            {
+                if (prod.Category == categoryParam) {
+                    yield return prod;
+                }
+            }
+        }
+
+    }
+}
---
+++ Chapter4_LanguageFeatures/Chapter4_LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chapter4_LanguageFeatures.Models
+{
+    public class ShoppingCart: IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
