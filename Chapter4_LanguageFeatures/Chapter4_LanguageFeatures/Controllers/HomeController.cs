4123686e4dcc8d8c04631bcd3aebac40f3965c32
Diogo Carlos Saramago Marques <diocar24@hotmail.com>
2017-12-10 13:48:59 +0000

    1) Performing Language Integrated Queries 1.b) Understanding Deferred LINQ Queries

--- Chapter4_LanguageFeatures/Chapter4_LanguageFeatures/Controllers/HomeController.cs
+++ Chapter4_LanguageFeatures/Chapter4_LanguageFeatures/Controllers/HomeController.cs
@@ -181,5 +181,123 @@ namespace Chapter4_LanguageFeatures.Controllers
 
             return View("Result", (object)result.ToString());
         }
+
+
+        public ViewResult FindProducts()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            // define the array to hold the results
+            Product[] foundProducts = new Product[3];
+            // sort the contents of the array
+            Array.Sort(products, (item1, item2) =>
+            {
+                // make it descending
+                return (-1) * Comparer<decimal>.Default.Compare(item1.Price, item2.Price);
+            });
+            // get the first three items in the array as the results
+            Array.Copy(products, foundProducts, 3);
+
+            // create the result
+            StringBuilder result = new StringBuilder();
+            foreach (Product p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult FindProductsLinqQuerySyntax()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var foundProducts = from match in products
+                                orderby match.Price descending
+                                select new { match.Name, match.Price };
+            // create the result
+            int count = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+                if (++count == 3)
+                {
+                    break;
+                }
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult FindProductsLinqDotNotation()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var foundProducts = products.OrderByDescending(e => e.Price)
+                                    .Take(3)
+                                    .Select(e => new { e.Name, e.Price });
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult FindProductsLinqDeferred()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var foundProducts = products.OrderByDescending(e => e.Price)
+                                    .Take(3)
+                                    .Select(e => new { e.Name, e.Price });
+
+            products[2] = new Product { Name = "Stadium", Price = 79600M };
+
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult SumProducts() {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var results = products.Sum(e => e.Price);
+
+            products[2] = new Product { Name = "Stadium", Price = 79500M };
+
+            return View("Result", (object)String.Format("Sum: {0:c}", results));
+        }
     }
 }
