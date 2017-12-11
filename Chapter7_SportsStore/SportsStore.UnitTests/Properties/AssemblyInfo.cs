25ae1092dfb94f63ecbd60b7b83356ff68e852f7
Diogo Carlos Saramago Marques <diocar24@hotmail.com>
2017-12-11 19:41:51 +0000

    1) Chapter7_SportsStore: solution start setup 1.b) SportsStore.Domain Project 1.c) SportsStore.WebUI Project 1.d) SportsStore.unitTests Project

---
+++ Chapter7_SportsStore/SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b68b17cc-dbfd-4dc3-9f9f-220ec304938a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
