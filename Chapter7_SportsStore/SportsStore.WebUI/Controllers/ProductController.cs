3a3621ad060f17bb8b14658055dfc4f6514c7c30
Diogo Carlos Saramago Marques <diocar24@hotmail.com>
2017-12-12 20:21:50 +0000

    1) Adding Pagination 1.b) UNIT TEST: PAGINATION 1.c) Displaying Page Links 1.d) Adding the View Model 1.e) Adding the HTML Helper Method (+ unit test) 1.f) Adding the View Model Data (+ unit tests) 1.g) Displaying the Page Links

--- Chapter7_SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ Chapter7_SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductsRepository productsRepository;
+        public int PageSize = 4;
 
         public ProductController(IProductsRepository productsRepositoryParam)
         {
@@ -17,9 +19,21 @@ namespace SportsStore.WebUI.Controllers
         }
 
         // GET: Product
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(productsRepository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = productsRepository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = productsRepository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
