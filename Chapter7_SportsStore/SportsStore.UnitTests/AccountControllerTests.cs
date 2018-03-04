26aa734cbe3db055f94e6a4378bea13ae4a4d3c6
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-04 23:02:28 +0000

    -> Chapter12_SportsStore:  Security & Finishing Touches - * Securing the Administration Controller 1) Creating a Basic Security Policy 2) Creating the Authentication Provider 3) Creating the Account Controller 4) Creating the View

---
+++ Chapter7_SportsStore/SportsStore.UnitTests/AccountControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AccountControllerTests // AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            // Arrange - create a mock authentication provider
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+
+            // Arrange - create the view model
+            var model = new LoginViewModel { UserName = "UN1", Password = "P1" };
+
+            // Arrange - Mock AUthenticate with credentials
+            mock.Setup(m => m.Authenticate(model.UserName, model.Password)).Returns(true);
+
+            // Arrange - create the controller
+            AccountController target = new AccountController(mock.Object);
+
+            // Act - authenticate using valid credentials
+            ActionResult resultWithRedirect = target.Login(model, "/MyURL");
+            ActionResult resultWithRedirectToAction = target.Login(model, null);
+
+            // Assert
+            mock.Verify(m => m.Authenticate("UN1", "P1"));
+            Assert.IsInstanceOfType(resultWithRedirect, typeof(RedirectResult));
+            Assert.AreEqual("/MyURL" , ((RedirectResult)resultWithRedirect).Url);
+
+            Assert.IsInstanceOfType(resultWithRedirectToAction, typeof(RedirectToRouteResult));
+            Assert.AreEqual("Index", ((RedirectToRouteResult)resultWithRedirectToAction).RouteValues["Action"]);
+            Assert.AreEqual("Admin", ((RedirectToRouteResult)resultWithRedirectToAction).RouteValues["Controller"]);
+        }
+
+        [TestMethod]
+        public void Cannot_Login_With_Invalid_Credentials()
+        {
+            // Arrange - create a mock authentication provider
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+
+            // Arrange - create the view model
+            var model = new LoginViewModel { UserName = "UN1", Password = "P1" };
+
+            // Arrange - Mock AUthenticate with credentials fails
+            mock.Setup(m => m.Authenticate(model.UserName, model.Password)).Returns(false);
+
+            // Arrange - create the controller
+            AccountController target = new AccountController(mock.Object);
+
+            // Act - authenticate using invalid credentials
+            ActionResult result = target.Login(model, null);
+
+            // Assert
+            mock.Verify(m => m.Authenticate("UN1", "P1"));
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+    }
+}
---
+++ Chapter7_SportsStore/SportsStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthProvider _authProvider;
+
+        public AccountController(IAuthProvider authProvider)
+        {
+            _authProvider = authProvider;
+        }
+
+        // GET: AccountController
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var authenticated = _authProvider.Authenticate(model.UserName, model.Password);
+
+                if (authenticated)
+                {
+                    if (string.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else {
+                        return Redirect(returnUrl);
+                     }                   
+                }
+                ModelState.AddModelError("", "Incorrect username or password");
+            }
+
+            return View(model);
+        }
+    }
+}
