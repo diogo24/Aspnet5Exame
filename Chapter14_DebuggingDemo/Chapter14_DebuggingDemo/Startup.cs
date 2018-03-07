eb68a2648909eb11afb6b6adebacb42df15aeff8
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-07 19:50:24 +0000

    * Debugging MVC Applications 1) Preparing the Example Project 2) Creating the Controller 3) Creating the View 4) Launching the Visual Studio Debugger * Causing the Visual Studio Debugger to Break 5) Using Breakpoints 6) Viewing Data Values in the Code Editor 7) Breaking on Exceptions * Using Edit and Continue 8) Enabling Edit and Continue 9) Modifying the Project 10) Editing and Continuing * Using Browser Link

---
+++ Chapter14_DebuggingDemo/Chapter14_DebuggingDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chapter14_DebuggingDemo.Startup))]
+namespace Chapter14_DebuggingDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
