8949caae709036644839c04c83c57d8840dce45d
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-07 19:23:18 +0000

    -> Chapter14:  Overview of MVC Projects - * Working with Visual Studio MVC Projects 1) Creating the Project 2) Understanding MVC Conventions 3) Following Conventions for Controller Classes 4) Following Conventions for Views 5) Following Conventions for Layouts

---
+++ OverviewMVCProject/OverviewMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OverviewMVCProject.Startup))]
+namespace OverviewMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
