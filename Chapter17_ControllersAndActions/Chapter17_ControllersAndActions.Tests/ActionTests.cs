17e223493bb7638cb6c6448e2374d1a22d61ac7f
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-09 22:48:45 +0000

    -> Chapter17: Controllers and Actions - * Preparing the Example Project 1) Setting the Start URL * Introducing the Controller 2) Creating a Controller with IController 3) Creating a Controller by Deriving from the Controller Class * Receiving Request Data 4) Getting Data from Context Objects 5) Understanding How Parameters Objects Are Instantiated 6) Specifying Default Parameter Values * Producing Output 7) Understanding Action Results 8) Returning HTML by Rendering a View 9) SPECIFYING A VIEW BY ITS PATH

---
+++ Chapter17_ControllersAndActions/Chapter17_ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using Chapter17_ControllersAndActions.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chapter17_ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ControllerTest()
+        {
+            // Arrange  - create the controller
+            var controller = new ExampleController();
+
+            // Act  - call the action method
+            ViewResult result = controller.Index();
+
+            // Assert
+            Assert.AreEqual("Homepage", result.ViewName);
+        }
+    }
+}
---
+++ Chapter17_ControllersAndActions/Chapter17_ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,53 @@
+﻿using Chapter17_ControllersAndActions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter17_ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: Derived
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+
+        public ActionResult ProduceOutput() {
+            //if (Server.MachineName == "TINY")
+            //{
+            //    //Response.Redirect("/Basic/Index");
+            //    return new CustomRedirectResult { Url = "/Basic/Index" };
+            //}
+            //else {
+            //    Response.Write("Controller: Derived, Action: ProduceOutput");
+            //    return null;
+            //}
+            return new RedirectResult("/Basic/Index");
+        }
+
+        public ActionResult ShowWeatherForecast()
+        {
+            string city = (string)RouteData.Values["city"];
+            DateTime forDate = DateTime.Parse(Request.Form["forDate"]);
+            // ... implement weather forecast here ...
+            return View();
+        }
+
+        public ActionResult ShowWeatherForecast(string city, DateTime forDate)
+        {
+            // ... implement weather forecast here ...
+            return View();
+        }
+
+        public ActionResult Search(string query = "all", int page = 1)
+        {
+            // ...process request...
+            return View();
+        }
+
+    }
+}
