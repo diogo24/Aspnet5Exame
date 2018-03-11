544fceb7a86a048b481a24b4d4369025a6771694
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-11 20:20:34 +0000

    * Creating Custom Helper Methods 1) Creating an Inline Helper Method 2) Creating an External Helper Method 3) Using a Custom External Helper Method4) KNOWING WHEN TO USE HELPER METHODS * Managing String Encoding in a Helper Method 5) Demonstrating the Problem 6) Encoding Helper Method Content

---
+++ Chapter21_HelperMethods/Chapter21_HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter21_HelperMethods.Infrastructure
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper htmlHelper, string[] items) {
+            TagBuilder tag = new TagBuilder("ul");
+            foreach (var str in items)
+            {
+                var li = new TagBuilder("li");
+                li.SetInnerText(str);
+                tag.InnerHtml += li.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+
+        //public static string DisplayMessage(this HtmlHelper htmlHelper, string msg) {
+        //    var result = String.Format("This is the message: <p>{0}</p>", msg);
+        //    //return new MvcHtmlString(result);
+        //    return result; 
+        //}
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper htmlHelper, string msg)
+        {
+            var encoded = htmlHelper.Encode(msg);
+            var result = String.Format("This is the message: <p>{0}</p>", encoded);
+            return new MvcHtmlString(result);
+        }
+    }
+}
