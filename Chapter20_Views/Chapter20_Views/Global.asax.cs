48218c499491184e9c08b392c28603953e6260f6
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-11 18:30:11 +0000

    -> Chapter20: Views - * Creating a Custom View Engine 1) Preparing the Example Project 2) Creating a Custom IView 3) Creating an IViewEngine Implementation 4) Registering a Custom View Engine 5) Testing the View Engine

---
+++ Chapter20_Views/Chapter20_Views/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using Chapter20_Views.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Chapter20_Views
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //ViewEngines.Engines.Add(new DebugDataViewEngine());
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new DebugDataViewEngine());
+        }
+    }
+}
