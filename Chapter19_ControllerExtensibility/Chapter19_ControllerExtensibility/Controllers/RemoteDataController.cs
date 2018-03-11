2b0223f731afb5dcd599c1f3143e3693211fa10c
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-11 18:01:47 +0000

    * Using Asynchronous Controllers 1) Creating the Example 2) Creating an Asynchronous Controller 3) Consuming Asynchronous Methods in a Controller

---
+++ Chapter19_ControllerExtensibility/Chapter19_ControllerExtensibility/Controllers/RemoteDataController.cs
@@ -0,0 +1,28 @@
+﻿using Chapter19_ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter19_ControllerExtensibility.Controllers
+{
+    public class RemoteDataController : Controller
+    {
+        // GET: RemoteData
+        public ActionResult Data()
+        {
+            RemoteService service = new RemoteService();
+            string data = service.GetRemoteData();
+            return View((object)data);
+        }
+
+        public async Task<ActionResult> Data_Asynchronous()
+        {
+            RemoteService service = new RemoteService();
+            string data = await service.GetRemoteDataAsync();
+            return View("Data", (object)data);
+        }
+    }
+}
---
+++ Chapter19_ControllerExtensibility/Chapter19_ControllerExtensibility/Models/RemoteService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Chapter19_ControllerExtensibility.Models
+{
+    public class RemoteService
+    {
+        internal string GetRemoteData()
+        {
+            Thread.Sleep(3000);
+            return "Hello from the other side of the world";
+        }
+
+        internal async Task<string> GetRemoteDataAsync()
+        {
+            return await Task<string>.Factory.StartNew(() => {
+                Thread.Sleep(3000);
+                return "Hello from the other side of the world async";
+            }) ;
+        }
+    }
+}
