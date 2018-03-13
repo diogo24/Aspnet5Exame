fea3091ccf1bb7dd8eb205e624d57f88716ae03f
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-13 17:47:24 +0000

    -> Chapter23: URL and Ajax Helper Methods - * Preparing the Example Project 1) Defining Additional CSS Styles

---
+++ Chapter21_HelperMethods/Chapter21_HelperMethods/Controllers/PeopleController.cs
@@ -0,0 +1,44 @@
+﻿using Chapter21_HelperMethods.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter21_HelperMethods.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private Person[] _personData = {
+             new Person {FirstName = "Adam", LastName = "Freeman", Role = Role.Admin},
+             new Person {FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User},
+             new Person {FirstName = "John", LastName = "Smith", Role = Role.User},
+             new Person {FirstName = "Anne", LastName = "Jones", Role = Role.Guest}
+         };        
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPeople()
+        {
+            return View(_personData);
+        }
+
+        [HttpPost]
+        public ActionResult GetPeople(string selectedRole)
+        {
+            if (string.IsNullOrEmpty(selectedRole) || selectedRole == "All")
+            {
+                return View(_personData);
+            }
+            else
+            {
+                Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                var list = _personData.Where(p => p.Role == selected);
+                return View(list);
+            }
+        }
+    }
+}
