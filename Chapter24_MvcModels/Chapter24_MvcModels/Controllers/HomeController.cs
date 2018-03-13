f0cabf36133a72976d33b90a61f38d092ec5ddbc
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-13 20:03:01 +0000

    -> Chapter24: Model Binding - * Preparing the Example Project

---
+++ Chapter24_MvcModels/Chapter24_MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Chapter24_MvcModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter24_MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] _personData = {
+             new Person {PersonId = 1, FirstName = "Adam", LastName = "Freeman",
+             Role = Role.Admin},
+             new Person {PersonId = 2, FirstName = "Jacqui", LastName = "Griffyth",
+             Role = Role.User},
+             new Person {PersonId = 3, FirstName = "John", LastName = "Smith",
+             Role = Role.User},
+             new Person {PersonId = 4, FirstName = "Anne", LastName = "Jones",
+             Role = Role.Guest}
+         };
+
+        // GET: Home
+        public ActionResult Index(int id)
+        {
+            Person person = _personData.Where(p => p.PersonId == id).First();
+            return View(person);
+        }
+    }
+}
