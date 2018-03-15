a390ff26c780dea40f65858c41640fbf45a6d1db
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-15 18:13:16 +0000

    -> Chapter25: Model Validation - * Preparing the Example Project 1) Creating the Layout 2) Creating the Views

---
+++ Chapter25_ModelValidation/Chapter25_ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Chapter25_ModelValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter25_ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            // statements to store new Appointment in a
+            // repository would go here in a real project
+            return View("Completed", appt);
+        }
+    }
+}
