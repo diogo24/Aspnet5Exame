81aea0c4b2174416de1d89f82dfbbfd695c91fca
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-15 19:38:02 +0000

    * Performing Client-Side Validation 1) Enabling Client-Side Validation 2) Adding the NuGet Packages 3) Using Client-Side Validation 4) Understanding How Client-Side Validation Works 5) AVOIDING CONFLICTS WITH BROWSER VALIDATION 6) MVC CLIENT VALIDATION VERSUS JQUERY VALIDATION 7) Performing Remote Validation

--- Chapter25_ModelValidation/Chapter25_ModelValidation/Models/Appointment.cs
+++ Chapter25_ModelValidation/Chapter25_ModelValidation/Models/Appointment.cs
@@ -2,48 +2,62 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Chapter25_ModelValidation.Models
 {
-    //[NoJoeOnMondays]
-    public class Appointment : IValidatableObject
+    ////[NoJoeOnMondays]
+    //public class Appointment : IValidatableObject
+    //{
+    //    //[Required]
+    //    public string ClientName { get; set; }
+
+    //    [DataType(DataType.Date)]
+    //    //[Required(ErrorMessage = "Please enter a date")]
+    //    //[FutureDate(ErrorMessage = "Please enter a date")]
+    //    public DateTime Date { get; set; }
+
+    //    //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
+    //    //[MustBeTrue(ErrorMessage = "You must accept the terms")]
+    //    public bool TermsAccepted { get; set; }
+
+    //    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    //    {
+    //        List<ValidationResult> errors = new List<ValidationResult>();
+
+    //        if (string.IsNullOrEmpty(ClientName))
+    //        {
+    //            errors.Add(new ValidationResult("Please enter your name", new List<string> { "ClientName" }));
+    //        }
+    //        if (DateTime.Now > Date)
+    //        {
+    //            errors.Add(new ValidationResult("Please enter a date in the future"));
+    //        }
+    //        if (errors.Count == 0 && ClientName == "Joe"
+    //        && Date.DayOfWeek == DayOfWeek.Monday)
+    //        {
+    //            errors.Add(
+    //            new ValidationResult("Joe cannot book appointments on Mondays"));
+    //        }
+    //        if (!TermsAccepted)
+    //        {
+    //            errors.Add(new ValidationResult("You must accept the terms"));
+    //        }
+
+    //        return errors;
+    //    }
+    //}
+
+    public class Appointment
     {
-        //[Required]
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
-        //[Required(ErrorMessage = "Please enter a date")]
-        //[FutureDate(ErrorMessage = "Please enter a date")]
+        [Remote("ValidateDate", "Home")]
         public DateTime Date { get; set; }
 
-        //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
-        //[MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            List<ValidationResult> errors = new List<ValidationResult>();
-
-            if (string.IsNullOrEmpty(ClientName))
-            {
-                errors.Add(new ValidationResult("Please enter your name", new List<string> { "ClientName" }));
-            }
-            if (DateTime.Now > Date)
-            {
-                errors.Add(new ValidationResult("Please enter a date in the future"));
-            }
-            if (errors.Count == 0 && ClientName == "Joe"
-            && Date.DayOfWeek == DayOfWeek.Monday)
-            {
-                errors.Add(
-                new ValidationResult("Joe cannot book appointments on Mondays"));
-            }
-            if (!TermsAccepted)
-            {
-                errors.Add(new ValidationResult("You must accept the terms"));
-            }
-
-            return errors;
-        }
     }
 }
