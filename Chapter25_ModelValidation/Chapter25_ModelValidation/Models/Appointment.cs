1e5aab194dc6018104fa67970fabfd2f26e0da4a
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-15 19:12:35 +0000

    1) Defining Self-Validating Models

--- Chapter25_ModelValidation/Chapter25_ModelValidation/Models/Appointment.cs
+++ Chapter25_ModelValidation/Chapter25_ModelValidation/Models/Appointment.cs
@@ -1,22 +1,49 @@
 ﻿using Chapter25_ModelValidation.Infrastructure;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Chapter25_ModelValidation.Models
 {
-    [NoJoeOnMondays]
-    public class Appointment
+    //[NoJoeOnMondays]
+    public class Appointment : IValidatableObject
     {
-        [Required]
+        //[Required]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
         //[Required(ErrorMessage = "Please enter a date")]
-        [FutureDate(ErrorMessage = "Please enter a date")]
+        //[FutureDate(ErrorMessage = "Please enter a date")]
         public DateTime Date { get; set; }
 
         //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
-        [MustBeTrue(ErrorMessage = "You must accept the terms")]
+        //[MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (string.IsNullOrEmpty(ClientName))
+            {
+                errors.Add(new ValidationResult("Please enter your name", new List<string> { "ClientName" }));
+            }
+            if (DateTime.Now > Date)
+            {
+                errors.Add(new ValidationResult("Please enter a date in the future"));
+            }
+            if (errors.Count == 0 && ClientName == "Joe"
+            && Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                errors.Add(
+                new ValidationResult("Joe cannot book appointments on Mondays"));
+            }
+            if (!TermsAccepted)
+            {
+                errors.Add(new ValidationResult("You must accept the terms"));
+            }
+
+            return errors;
+        }
     }
 }
