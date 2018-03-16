abf083f053d079ce0e22c465f1735e98c208bb81
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-16 22:26:01 +0000

    * Profiling Script and Style Sheet Loading * Using Script and Style Bundles 1) Adding the NuGet Package 2) Defining the Bundles 3) Applying Bundles 4) Optimizing the JavaScript and CSS Files

---
+++ Chapter26_ClientFeatures/Chapter26_ClientFeatures/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Chapter26_ClientFeatures
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundlers(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/content/*.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/clientfeaturesscripts")
+             .Include("~/Scripts/jquery-{version}.js",
+             "~/Scripts/jquery.validate.js",
+             "~/Scripts/jquery.validate.unobtrusive.js",
+             "~/Scripts/jquery.unobtrusive-ajax.js"));
+        }
+
+    }
+}
